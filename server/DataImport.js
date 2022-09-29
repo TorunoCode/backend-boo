d999882f22c6de68babf0d45618e598099745f68
thanh lam <thanhlam110230@gmail.com>
2022-09-29 18:16:01 +0700

    update

--- server/DataImport.js
+++ server/DataImport.js
@@ -3,17 +3,19 @@ import User from './models/userModel.js';
 import users from './data/users.js';
 import movies  from './data/movies.js';
 import Movie from './models/movieModel.js';
-
+import asyncHandler from 'express-async-handler';
 const ImportData = express.Router();
 
-ImportData.post("/user",async(req,res)=>{
+ ImportData.post("/user",
+ asyncHandler(async(req,res)=>{
     await User.remove({});
     const importUser = await User.insertMany(users);
     res.send({importUser});
-});
-ImportData.post("/movies",async(req,res)=>{
+}));
+ImportData.post("/movies",
+asyncHandler(async(req,res)=>{
     await Movie.remove({});
     const importMovie= await Movie.insertMany(movies);
     res.send({importMovie});
-});
+}));
 export default ImportData;
