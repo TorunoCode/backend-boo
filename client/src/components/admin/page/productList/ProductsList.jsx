6835805d862776338366363794b8a08c3cc0c4e7
thanh lam <thanhlam110230@gmail.com>
2022-10-20 16:42:34 +0700

    update day 8

--- client/src/components/admin/page/productList/ProductsList.jsx
+++ client/src/components/admin/page/productList/ProductsList.jsx
@@ -1,62 +1,105 @@
 import './productList.css'
 import { DataGrid} from '@mui/x-data-grid';
-import { productRows } from '../../dummyData';
 import { Link } from 'react-router-dom';
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
+import TopBar from '../../components/topbar/TopBar';
+import Sidebar from "../../components/sidebar/Sidebar";
+import axios from "axios";
+
 export default function ProductsList() {
-  const [data,setData] =useState(productRows);
+  const [data,setData] =useState([]);
   const handleDelete = (id)=> {
     setData(data.filter((item)=>item.id !== id));
   };
+  function convert(str) {
+    var date = new Date(str),
+      mnth = ("0" + (date.getMonth() + 1)).slice(-2),
+      day = ("0" + date.getDate()).slice(-2);
+    return [ mnth, day,date.getFullYear()].join("/");
+  }
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const {data} = await axios.get("/api/movies");
+      setData(data);
+    };
+    fetchUsers();
+  }, []);
     const columns = [
-  { field: 'id', headerName: 'ID', width: 60 },
-  { field: 'product', headerName: 'Product', width: 160, renderCell:(params) =>{
-    return (
-      <div className="productListItem">
-        <img className='productListImg' src={params.row.avatar} alt=""/>
-        {params.row.name}
+  { field: '_id', headerName: 'ID', width: 60 },
+  { field: 'name', headerName: 'NAME',width: 240, renderCell: (params)=>{
+    return(
+      <>
+      <div className='media align-items-center'>
+      <div className='iq-movie'>
+        <a>
+          <img src={params.row.image} className='img-border-radius avatar-40 img-fluid' />
+        </a>
       </div>
-    )
+      <div className='media-body text-left ml-3'>
+        <p className='mb-0'>{params.row.name}</p>
+        <small>{params.row.runningTime}</small>
+      </div>
+      </div>
+      </>
+    );
   } },
-  { field: 'stock', headerName: 'Stock', width: 160 },
+  { field: 'genre', headerName: 'CATEGORY', width: 160},
+  { field: 'releaseTime', headerName: 'RELEASE YEAR	', width: 160 ,sortable: false
+  , renderCell: (params)=>{
+    return(
+      <>
+     <p> {convert(params.row.releaseTime)}</p>
+      </>
+    );
+  }},
+  { field: 'language', headerName: 'LANGUAGE', width: 160 },
   {
-    field: 'status',
-    headerName: 'Status',
-    width: 120,
+    field: 'describe',
+    headerName: 'DESCRIPTION',
+    description: 'This column has a value getter and is not sortable.',width: 160
   },
   {
-    field: 'price',
-    headerName: 'Price',
-    description: 'This column has a value getter and is not sortable.',
-    sortable: false,
-    width: 130
-    },
-    {
-      field:'action', headerName:'Action', width:150, renderCell: (params)=>{
-        return(
-          <>
-          <Link to={'/product/'+params.row.id}>
-          <button className="productListEdit">Edit</button>
-          </Link>
-          <svg  onClick={()=>handleDelete(params.row.id)} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 productListDelete">
-  <path strokeLinecap="round" strokeLinejoin="round" d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+    field:'action', headerName:'ACTION', width:150, renderCell: (params)=>{
+      return(
+        <>
+        <Link to={'/user/'+params.row.id}>
+        <button className="userListEdit">Edit</button>
+        </Link>
+        <div className='icon'>
+        <a className='iq-bg-primary'>
+        <svg onClick={()=>handleDelete(params.row.id)} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 userListDelete" >
+  <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
 </svg>
-
-          </>
-        );
-      }
+</a>
+</div>
+        </>
+      );
     }
+  }
 ];
+
+
   return (
-    <div className='productList'>
-      <DataGrid
+    <div className="body">
+      <TopBar/>
+     <div className="container">
+      <Sidebar/>
+    <div className="userList">
+
+    <div class="iq-header-title "> <h4 class="card-title">Editable Table</h4> </div>
+    <div class="iq-card-header-toolbar d-flex align-items-center"><a class="btn btn-primary" href="/HomeAdmin/newProduct">Add movie</a></div>
+       <DataGrid
       rows={data}
+      getRowId={(row) => row._id} 
       disableSelectionOnClick
       columns={columns}
       pageSize={8}
       rowsPerPageOptions={[5]}
       checkboxSelection
     />
-    </div>
-  )
+  </div>
+  </div>
+  <div className='test'></div>
+  </div>
+  );
 }
--- client/src/components/admin/page/userList/UserList.jsx
+++ client/src/components/admin/page/userList/UserList.jsx
@@ -1,55 +1,66 @@
 import './userList.css'
 import { DataGrid} from '@mui/x-data-grid';
-import { userRows } from '../../dummyData';
 import { Link } from 'react-router-dom';
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 import TopBar from '../../components/topbar/TopBar';
 import Sidebar from "../../components/sidebar/Sidebar";
+import axios from "axios";
 
 export default function UserList() {
-  const [data,setData] =useState(userRows);
+  const [data,setData] =useState([]);
   const handleDelete = (id)=> {
     setData(data.filter((item)=>item.id !== id));
   };
+  function convert(str) {
+    var date = new Date(str),
+      mnth = ("0" + (date.getMonth() + 1)).slice(-2),
+      day = ("0" + date.getDate()).slice(-2);
+    return [ mnth, day,date.getFullYear()].join("/");
+  }
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const {data} = await axios.get("/api/user");
+      setData(data);
+    };
+    fetchUsers();
+  }, []);
     const columns = [
-  { field: 'id', headerName: 'ID', width: 60 },
-  { field: 'user', headerName: 'User', width: 130, renderCell:(params) =>{
-    return (
-      <div className="userListUser">
-        <img className='userListImg' src={params.row.avatar} alt=""/>
-        {params.row.username}
-      </div>
-    )
-  } },
-  { field: 'username', headerName: 'Username', width: 160 },
-  { field: 'email', headerName: 'Email', width: 160 },
+  { field: '_id', headerName: 'ID', width: 60 },
+  { field: 'name', headerName: 'NAME', width: 120 },
+  { field: 'email', headerName: 'EMAIL', width: 160},
+  { field: 'isActive', headerName: 'ACTIVE', width: 160 },
+  { field: 'isAdmin', headerName: 'ADMIN', width: 160 },
   {
-    field: 'status',
-    headerName: 'Status',
-    width: 120,
+    field: 'createdAt',
+    headerName: 'CREATED',
+    description: 'This column has a value getter and is not sortable.',
+    sortable: false
+    , renderCell: (params)=>{
+      return(
+        <>
+       <p> {convert(params.row.createdAt)}</p>
+        </>
+      );
+    }
   },
   {
-    field: 'transaction',
-    headerName: 'Transaction',
-    description: 'This column has a value getter and is not sortable.',
-    sortable: false,
-    width: 130
-    },
-    {
-      field:'action', headerName:'Action', width:150, renderCell: (params)=>{
-        return(
-          <>
-          <Link to={'/user/'+params.row.id}>
-          <button className="userListEdit">Edit</button>
-          </Link>
-          <svg onClick={()=>handleDelete(params.row.id)} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 userListDelete">
-  <path strokeLinecap="round" strokeLinejoin="round" d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+    field:'action', headerName:'ACTION', width:150, renderCell: (params)=>{
+      return(
+        <>
+        <Link to={'/user/'+params.row.id}>
+        <button className="userListEdit">Edit</button>
+        </Link>
+        <div className='icon'>
+        <a className='iq-bg-primary'>
+        <svg onClick={()=>handleDelete(params.row.id)} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 userListDelete" >
+  <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
 </svg>
-
-          </>
-        );
-      }
+</a>
+</div>
+        </>
+      );
     }
+  }
 ];
 
 
@@ -59,8 +70,11 @@ export default function UserList() {
      <div className="container">
       <Sidebar/>
     <div className="userList">
+    <div class="iq-header-title "> <h4 class="card-title">Editable Table</h4> </div>
+    <div class="iq-card-header-toolbar d-flex align-items-center"><a class="btn btn-primary" href="/streamit/dashboard/react/build/add-movie">Add movie</a></div>
     <DataGrid
       rows={data}
+      getRowId={(row) => row._id} 
       disableSelectionOnClick
       columns={columns}
       pageSize={8}
@@ -69,6 +83,7 @@ export default function UserList() {
     />
   </div>
   </div>
+  <div className='test'></div>
   </div>
-  )
+  );
 }
