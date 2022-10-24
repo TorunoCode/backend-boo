ce9b5a1b8471ce761508d59d0b117d8950d3f6cb
19110026-Nguyen-Huu-Duc-Thanh <19110026@student.hcmute.edu.vn>
2022-10-24 21:03:26 +0700

    update timezone

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ import commentsFeedback from './routes/commentsFeedbacksRoutes.js';
 dotenv.config();
 connectDatabase();
 const app = express();
+process.env.TZ = "Asia/Ho_Chi_Minh";
 app.use(cors());
 app.use(bodyParser.json({limit:"30mb",extended:true}));
 app.use(bodyParser.urlencoded({limit:"30mb",extended:true}));
