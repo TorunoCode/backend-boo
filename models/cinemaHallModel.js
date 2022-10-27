fff8bea145ea6aece40c5da8041ff57d8d394989
TorunoCode <104509788+TorunoCode@users.noreply.github.com>
2022-10-27 15:26:20 +0700

    update CinemaHall

---
+++ models/cinemaHallModel.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+const cinemaHallSchema =  new mongoose.Schema({
+    name:{
+        type: String,
+        require:true
+    },
+    totalSeats:{
+        type: String,
+        require:true
+    },
+    idCinema:{
+        type: String,
+        require:true
+    },
+
+});
+const CinemaHall =  mongoose.model('CinemaHall',cinemaHallSchema);
+export default CinemaHall;
