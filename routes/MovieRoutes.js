05da8d63fd188cf3c8e21535285257a4cd8136e0
thanhlam <thanhlam110230@gmail.com>
2022-12-03 10:35:53 +0700

    update 22

--- routes/MovieRoutes.js
+++ routes/MovieRoutes.js
@@ -514,7 +514,7 @@ function convert(str) {
     var date = new Date(str),
       mnth = ("0" + (date.getMonth() + 1)).slice(-2),
       day = ("0" + date.getDate()).slice(-2);
-    return [date.getFullYear(), mnth, day].join("-");
+    return [day,mnth,date.getFullYear()].join("-");
   }
 movieRoute.get(
     "/detailBooking/:id",
