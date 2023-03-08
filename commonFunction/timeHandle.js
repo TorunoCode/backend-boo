5bfda1b739d210f1d480b09712c47baa245ee4ac
19110026-Nguyen-Huu-Duc-Thanh <19110026@student.hcmute.edu.vn>
2023-03-08 21:20:45 +0700

    add time function clean summing routes

--- commonFunction/timeHandle.js
+++ commonFunction/timeHandle.js
@@ -1,5 +1,50 @@
 function checkTimeDifferenceMinute(time, timeCheck) {
-    console.log(((timeCheck - time) / 1000) / 60)
     return ((timeCheck - time) / 1000) / 60
 }
-export default { checkTimeDifferenceMinute }
+function getTodayAt0(time) {
+    time.setHours(0);
+    time.setMinutes(0);
+    time.setSeconds(0);
+    return new Date(time)
+}
+function getYesterdayAt0(time) {
+    let todayAt0 = getTodayAt0(time)
+    return new Date(todayAt0.setDate(todayAt0.getDate() - 1));
+}
+function getTomorrorAt0(time) {
+    let todayAt0 = getTodayAt0(time)
+    return new Date(todayAt0.setDate(todayAt0.getDate() + 1));
+}
+function getFirstDayOfMonth(time) {
+    return new Date(time.getFullYear(), time.getMonth(), 1);
+}
+function getFirstDayOfLastMonth(time) {
+    let firstDayOfMonth = getFirstDayOfMonth(time);
+    return new Date(firstDayOfMonth.setMonth(firstDayOfMonth.getMonth() - 1));
+}
+function getFirstDayOfNextMonth(time) {
+    let firstDayOfMonth = getFirstDayOfMonth(time);
+    return new Date(firstDayOfMonth.setMonth(firstDayOfMonth.getMonth() + 1));
+}
+function getFirstDayOfThisYear(time) {
+    return new Date(time.getFullYear(), 1, 1);
+}
+function getFirstDayOfNextYear(time) {
+    let firstDayOfNextYear = getFirstDayOfThisYear(time)
+    return new Date(firstDayOfNextYear.setFullYear(firstDayOfNextYear.getFullYear() + 1))
+}
+function getFirstDayOfWeek(time) {
+    let todayAt0 = getTodayAt0(time)
+    var day = todayAt0.getDay(),
+        diff = todayAt0.getDate() - day + (day == 0 ? -6 : 1); // adjust when day is sunday
+    return new Date(todayAt0.setDate(diff));
+}
+function getFirstDayOfNextWeek(time) {
+    let firstDayOfWeek = getFirstDayOfWeek(time)
+    return new Date(firstDayOfWeek.setDate(firstDayOfWeek.getDate() + 7))
+}
+export default {
+    checkTimeDifferenceMinute, getYesterdayAt0, getFirstDayOfMonth, getFirstDayOfLastMonth,
+    getFirstDayOfNextYear, getFirstDayOfThisYear, getTodayAt0, getTomorrorAt0,
+    getFirstDayOfNextMonth, getFirstDayOfWeek, getFirstDayOfNextWeek
+}
