f02ebe2e3866a73ceaf93a52ed09efb698feff49
19110026-Nguyen-Huu-Duc-Thanh <19110026@student.hcmute.edu.vn>
2023-03-08 19:51:25 +0700

    remove google signup

--- routes/oAuthGoogleRoutes.js
+++ routes/oAuthGoogleRoutes.js
@@ -49,50 +49,7 @@ async function verifyGoogleAccessToken(token) {
     });
     return dataToUse;
 }
-app.post("/Signup", async (req, res) => {
-    try {
-        // console.log({ verified: verifyGoogleToken(req.body.credential) });
-        console.log("tohere")
-        if (req.body.credential) {
-            const verificationResponse = await verifyGoogleToken(req.body.credential);
 
-            if (verificationResponse.error) {
-                return res.status(400).json({
-                    message: verificationResponse.error
-                });
-            }
-
-            const profile = verificationResponse?.payload;
-            console.log(profile)
-            const oldUser = await UserModal.findOne({ email: profile?.email });
-            if (!oldUser) {
-                const email = profile?.email;
-                const given_name = profile?.given_name;
-                const data = await UserModal.create({
-                    email: email, password: "", name: given_name, pin: "",
-                    isActive: true, isAdmin: false, fullName: profile?.family_name + profile?.given_name
-                })
-            }
-
-            res.status(201).json({
-                message: "Signup was successful",
-                user: {
-                    firstName: profile?.given_name,
-                    lastName: profile?.family_name,
-                    picture: profile?.picture,
-                    email: profile?.email,
-                    token: jwt.sign({ email: profile?.email }, "myScret", {
-                        expiresIn: "1d",
-                    }),
-                },
-            });
-        }
-    } catch (error) {
-        res.status(500).json({
-            message: error
-        });
-    }
-});
 // server.js
 app.post("/login", async (req, res) => {
     try {
