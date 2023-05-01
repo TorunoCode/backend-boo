c9471982dc8a78b5aef8ff85ec60330a63ffad31
19110026-Nguyen-Huu-Duc-Thanh <19110026@student.hcmute.edu.vn>
2023-05-01 09:01:16 +0700

    cors fix

--- index.js
+++ index.js
@@ -55,7 +55,9 @@ app.use(
 
 
 process.env.TZ = "Asia/Ho_Chi_Minh";
-app.use(cors());
+app.use(cors({
+  origin: ['https://web-fixgo.vercel.app', 'https://admin-fixgo.vercel.app', 'http://localhost:3000']
+}));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
