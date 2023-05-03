93505733244c9acc12c2f01c64ae24f3a0d2a39d
19110026-Nguyen-Huu-Duc-Thanh <19110026@student.hcmute.edu.vn>
2023-05-03 15:07:24 +0700

    fix cors

--- index.js
+++ index.js
@@ -59,10 +59,20 @@ app.use(express.json());
 
 
 process.env.TZ = "Asia/Ho_Chi_Minh";
+var allowedDomains = ['https://web-fixgo.vercel.app', 'https://admin-fixgo.vercel.app', 'http://localhost:3000',
+  'https://backend-boo.vercel.app', 'http://localhost:5000']
 app.use(cors({
   credentials: true,
-  origin: ['https://web-fixgo.vercel.app', 'https://admin-fixgo.vercel.app', 'http://localhost:3000',
-    'https://backend-boo.vercel.app', 'http://localhost:5000']
+  origin: function (origin, callback) {
+    // bypass the requests with no origin (like curl requests, mobile apps, etc )
+    if (!origin) return callback(null, true);
+
+    if (allowedDomains.indexOf(origin) === -1) {
+      var msg = `This site ${origin} does not have an access. Only specific domains are allowed to access it.`;
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
 }));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
