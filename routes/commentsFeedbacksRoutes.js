15b3604844bebb43d26164f178f60635b5cc12e1
19110026-Nguyen-Huu-Duc-Thanh <19110026@student.hcmute.edu.vn>
2023-05-03 11:23:39 +0700

    test

--- routes/commentsFeedbacksRoutes.js
+++ routes/commentsFeedbacksRoutes.js
@@ -48,7 +48,7 @@ app.post("/delete_allfeedback", async (request, response) => {
 });*/
 app.post("/add_feedback", async (request, response) => {
     if (!request.session.userId)
-        return response.status(400).json({ data: null, message: "Login First" + request.session });
+        return response.status(400).json({ data: null, message: "Login First" + request.session.userId });
     return response.status(400).json({ data: null, message: request.session });
     const feedback = new feedbacksModel(request.body);
     feedback.userId = request.session.userId;
