992023ccb8c9960fd5927549775701a10c03243d
19110026-Nguyen-Huu-Duc-Thanh <19110026@student.hcmute.edu.vn>
2023-05-03 13:40:16 +0700

    test

--- index.js
+++ index.js
@@ -38,6 +38,8 @@ app.use(
     cookie: {
       maxAge: 1000 * 60 * 12 * 24, // 1 week
       //sameSite: "none", // in order to response to both first-party and cross-site requests
+      domain: "backend-boo.vercel.app",
+      sameSite: "none",
     },
     store: store,
     resave: true,
