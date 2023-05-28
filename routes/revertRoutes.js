18ecabd8514fc8893ea9044b8628eab0ac5a8d60
thanhlam <thanhlam110230@gmail.com>
2023-05-28 22:58:26 +0700

    previously refunded

--- routes/revertRoutes.js
+++ routes/revertRoutes.js
@@ -48,6 +48,8 @@ app.post(
         const b = moment(req.body.createdAt, 'YYYY-MM-DD');
         const day = b.diff(a, 'days');
         for (let c of req.body.list) {
+            const check =  await revertModal.find({idShowSeat:c});
+            if(check) res.send("previously refunded");
             const order = await orderModel.findOne({ idBill: req.body.idBill, idShowSeat: c }, { idShowing: 1 });
             const seat = await showSeatModel.findById(c,{number:1});
             if (day >= 2) {
